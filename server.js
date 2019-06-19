9acad9f82ab2d4ef4a8a4a16217a437971c6c70e
onoroff <adam.grunke@gmail.com>
2019-06-19 13:43:11 -0700

    complete

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const fs = require('fs'); // file system aka your local hard drive.
-
+const methodOverride = require('method-override')
 const PORT = 3000;
 
 const app = express();
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(layouts);
 app.use(express.static(__dirname + "/static"));
+app.use(methodOverride('_method'));
 
 
 
@@ -27,11 +28,18 @@ app.get('/articles/new', function(req, res){
     res.render('articles/new');
 });
 
+app.get('/articles/:id/edit', function(req, res){
+    let articles = fs.readFileSync('./articles.json');
+    let articleData = JSON.parse(articles);
+    var id = parseInt(req.params.id);    
+    res.render('articles/edit', {articleData: articleData[id], id});
+});
+
 app.get('/articles/:id', function(req, res) {
-    var articles = fs.readFileSync('./articles.json');
-    var articleData = JSON.parse(articles);
+    let articles = fs.readFileSync('./articles.json');
+    let articleData = JSON.parse(articles);
     var id = parseInt(req.params.id);
-    res.render('articles/show', {article: articleData[id]});
+    res.render('articles/show', {article: articleData[id], id});
 });
 
 app.post('/articles/', function(req, res){
@@ -44,6 +52,37 @@ app.post('/articles/', function(req, res){
     articleData.push(newArticle);
     fs.writeFileSync('./articles.json', JSON.stringify(articleData));
     res.redirect('/articles');
+});
+
+
+// app.delete....
+
+app.delete('/articles/:id', function(req, res) {
+    //read the data from the file 
+    let articles = fs.readFileSync('./articles.json');
+    //parse the data into an object
+    var articleData = JSON.parse(articles);
+    // splice out the item at the specified index
+    var id= parseInt(req.params.id);
+    articleData.splice(id, 1);
+    //stringify rthe object
+    var articleString = JSON.stringify(articleData);
+    // write the object back to the file
+    fs.writeFileSync('./articles.json', articleString);
+    res.redirect('/articles');
+});
+
+app.put('/articles/:id', function(req, res){
+    let articles = fs.readFileSync('./articles.json');
+    let articleData = JSON.parse(articles);
+    var id = parseInt(req.params.id);
+
+    articleData[id].title = req.body.articleTitle;
+    articleData[id].body = req.body.articleBody;
+
+    fs.writeFileSync('./articles.json', JSON.stringify(articleData));
+    res.redirect('/articles/' + id);
+    
 });
 
 app.listen(PORT || 3000);
